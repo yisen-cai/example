55eec3ee62437c104c5eb5487a62d223f005700c
YISHEN CAI <yisen614@gmail.com>
2022-09-17 21:55:47 +0800

    new commit

--- src/test/java/com/glancebar/leetcode/PseudoPalindromicTest.java
+++ src/test/java/com/glancebar/leetcode/PseudoPalindromicTest.java
@@ -2,7 +2,6 @@ package com.glancebar.leetcode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.glancebar.datatype.TreeNode;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
@@ -12,18 +11,10 @@ class PseudoPalindromicTest {
 
   @Test
   void isPalindromic() {
+    System.out.println("hello world");
     assertFalse(pseudoPalindromic.isPalindromic(Arrays.asList(2, 3, 1)));
   }
 
   @Test
-  void pseudoPalindromicPaths() {
-    TreeNode<Integer> treeNode = new TreeNode<>(2);
-    treeNode.setRight(new TreeNode<>(1));
-    treeNode.getRight().setRight(new TreeNode<>(1));
-
-    treeNode.setLeft(new TreeNode<>(3));
-    treeNode.getLeft().setLeft(new TreeNode<>(3));
-    treeNode.getLeft().setRight(new TreeNode<>(1));
-    pseudoPalindromic.pseudoPalindromicPaths(treeNode);
-  }
+  void pseudoPalindromicPaths() {}
 }
