817cd27311d02a0cd0d3fbbdf396fe4bba94a2db
YISHEN CAI <yisen614@gmail.com>
2022-10-08 10:57:53 +0800

    update junit examples

---
+++ src/test/java/com/glancebar/junit/MetaAnnotationTests.java
@@ -0,0 +1,17 @@
+package com.glancebar.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class MetaAnnotationTests {
+
+    @Fast
+    @Test
+    void myFastTest() {
+        // ...
+    }
+
+    @FastTest
+    void myFastTest1() {
+        // ...
+    }
+}
